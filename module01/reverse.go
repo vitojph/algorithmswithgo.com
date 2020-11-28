d582259ea47439d977aa3364378fa43d402129cf
Víctor Peinado <vitojph@gmail.com>
2020-11-28 15:10:02 +0100

    Add solution for Reverse

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,11 +1,26 @@
 package module01
 
+import "strings"
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
 //   Reverse("cat") => "tac"
 //   Reverse("alphabet") => "tebahpla"
 //
+
+/*func Reverse(word string) string {
+	reverse := make([]string, 0)
+	for i := len(word) - 1; i >= 0; i-- {
+		reverse = append(reverse, string(word[i]))
+	}
+	return strings.Join(reverse, "")
+}*/
+
 func Reverse(word string) string {
-	return ""
+	var reverse strings.Builder
+	for i := len(word) - 1; i >= 0; i-- {
+		reverse.WriteByte(word[i])
+	}
+	return reverse.String()
 }
