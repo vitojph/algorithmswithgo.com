22abbb0683d0882f6eed4534a449f36631291037
Víctor Peinado <vitojph@gmail.com>
2020-11-29 15:52:37 +0100

    Add solution for FizzBuzz

--- module01/fizz_buzz.go
+++ module01/fizz_buzz.go
@@ -1,5 +1,11 @@
 package module01
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // FizzBuzz will print out all of the numbers
 // from 1 to N replacing any divisible by 3
 // with "Fizz", and divisible by 5 with "Buzz",
@@ -11,4 +17,17 @@ package module01
 // I wouldn't normally recommend this, but did
 // it here to make life easier for beginners.
 func FizzBuzz(n int) {
+	values := make([]string, 0)
+	for i := 1; i <= n; i++ {
+		if i%15 == 0 {
+			values = append(values, "Fizz Buzz")
+		} else if i%3 == 0 {
+			values = append(values, "Fizz")
+		} else if i%5 == 0 {
+			values = append(values, "Buzz")
+		} else {
+			values = append(values, strconv.Itoa(i))
+		}
+	}
+	fmt.Println(strings.Join(values, ", "))
 }
