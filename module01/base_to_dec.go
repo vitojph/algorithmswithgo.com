175ee014988e9703ce415d422d4daf6558f36f20
Víctor Peinado <vitojph@gmail.com>
2020-12-05 13:29:34 +0100

    Add solution for BaseToDec

--- module01/base_to_dec.go
+++ module01/base_to_dec.go
@@ -1,5 +1,10 @@
 package module01
 
+import (
+	"math"
+	"strings"
+)
+
 // BaseToDec takes in a number and the base it is currently
 // in and returns the decimal equivalent as an integer.
 //
@@ -8,6 +13,14 @@ package module01
 //   BaseToDec("E", 16) => 14
 //   BaseToDec("1110", 2) => 14
 //
-func BaseToDec(value string, base int) int {
-	return 0
+
+func BaseToDec(input string, base int) int {
+	const charset = "0123456789ABCDEF"
+	var result, power float64
+	for i := len(input) - 1; i >= 0; i-- {
+		value := float64(strings.Index(charset, string(input[i])))
+		result += value * math.Pow(float64(base), power)
+		power++
+	}
+	return int(result)
 }
