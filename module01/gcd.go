4a7da04a3a911be86faf9ae3c1d242887735bd90
Víctor Peinado <vitojph@gmail.com>
2020-12-06 15:45:24 +0100

    Add solution for Greatest Common Divisor using Euclidean algorithm

--- module01/gcd.go
+++ module01/gcd.go
@@ -1,5 +1,6 @@
 package module01
 
+/*
 func GCD(a, b int) int {
 	primes := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
 	var common []int
@@ -31,4 +32,14 @@ func GCD(a, b int) int {
 	}
 	return total
 
+}
+*/
+
+func GCD(a, b int) int {
+	// Euclidean algorithm
+	if b == 0 {
+		return a
+	}
+	a, b = b, a%b
+	return GCD(a, b)
 }
