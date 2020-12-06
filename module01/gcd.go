df2e5b54c1cb41d453099a6d47a3e29340c3273c
Víctor Peinado <vitojph@gmail.com>
2020-12-06 13:28:37 +0100

    Add solution for Greatest Common Divisor using factors

--- module01/gcd.go
+++ module01/gcd.go
@@ -1,5 +1,34 @@
 package module01
 
 func GCD(a, b int) int {
-	return 0
+	primes := []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
+	var common []int
+
+	// save the factors as a map
+	factorsA := make(map[int]int)
+	factorsB := make(map[int]int)
+	for _, n := range Factor(primes, a) {
+		factorsA[n]++
+	}
+	for _, n := range Factor(primes, b) {
+		factorsB[n]++
+	}
+
+	for k1, v1 := range factorsA {
+		for k2, v2 := range factorsB {
+			if k1 == k2 && v1 > 0 && v2 > 0 {
+				common = append(common, k1)
+				factorsA[k1]--
+				factorsB[k2]--
+				break
+			}
+		}
+	}
+
+	total := 1
+	for _, n := range common {
+		total *= n
+	}
+	return total
+
 }
